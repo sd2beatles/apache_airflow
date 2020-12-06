fdbc544988edf9be69ede1a9129041259a83d218
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2020-12-06 11:27:19 +0900

    Update managingData.py

--- practice/Lesson1/managingData.py
+++ practice/Lesson1/managingData.py
@@ -1,8 +1,8 @@
-
 from airflow import DAG
 from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.hive_operator import HiveOperator
 from datetime import datetime
 
 import fetching_tweet
@@ -10,6 +10,8 @@ import cleaning_tweet
 
 source_dir='replace me'
 destination_dir="replace me"
+path="replace me"
+table="replace me"
 
 default_args={
     "start_date":datetime(2020,1,1),
@@ -21,5 +23,5 @@ with DAG(dag_id="twitter_dag",default_args=default_args,schedule_interval="@dail
     fetching_tweet=PythonOperator(task_id="fetching_for_tweet",fs_conn_id="fs_tweet",python_callable=fetcing_tweet.main)
     cleaning_tweet=PythonOperator(task_id="cleaning_for_tweet",fs_conn_id="fs_tweet",python_callable=cleaning_tweet.main)
     storing_tweet=BashOperator(task_id="storing_for_tweet",bash_commnad=f"hadoop fs put -f {source_dir} {destination_dir}")
-
-
+    loading_tweet=HiveOperator(task_id="laoding_for_tweet",hql=f"LOAD DATA INPATH {path} INTO TABLE {table}")
+    
