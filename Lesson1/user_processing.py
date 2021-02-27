1b5c4e34a30f4799219cb33a83e9bcbf56426be8
sd2beatles <53164959+sd2beatles@users.noreply.github.com>
2021-02-27 18:56:22 +0900

    Update user_processing.py

--- Lesson1/user_processing.py
+++ Lesson1/user_processing.py
@@ -7,75 +7,65 @@ from airflow.operators.bash import BashOperator
 from pandas import json_normalize
 import json
 
-from datetime import datetime
-default_args={
-    "start_date":datetime.now}
+defaultParams={'function':'OVERVIEW',
+        'symbol':'IBM',
+        'apikey':"OPF63VL4JTRN9IR8"}
 
-def _processing_user(ti):
-    users=ti.xcom_pull(task_ids=['extracting_user'])
-    if not len(users) or 'results' not in users[0]:
-        raise ValueError("User is empty")
 
-    user=users[0]['results'][0]
-    processed_user=json_normalize({
-        'first_name':user['name']['first'],
-        'last_name':user['name']['last'],
-        'country':user['location']['country'],
-        'username':user['login']['username'],
-        'password':user['login']['password'],
-        'emial':user['email']
+def processing_companies(ti):
+    company=ti.xcom_pull(task_ids=['extracting_company'])[0]
+    if "Symbol" not in company:
+        raise ValueError("Loaind is unsuccessful")
+    proccessed_user=json_normalize({
+        "symbol":company["Symbol"],
+        "type":company[ "AssetType"],
+        "name":company["Name"]
     })
-    processed_user.to_csv("/tmp/processed_user.csv",index=None,header=False)
-
-    
+    proccessed_user.to_csv("/tmp/companies.csv",index=None,header=False)
 
 
-
-
-
-with DAG("user_processing",schedule_interval="@daily",start_date=datetime(2020,1,1),catchup=True) as dag:
-    #Define operator 
+with DAG("user_processing",schedule_interval="@daily",start_date=datetime(2021,2,27),catchup=False) as dag:
     creating_table=SqliteOperator(
         task_id="creating_table",
-        sqlite_conn_id="db_sqlite",
+        sqlite_conn_id="db_finance",
         sql='''
-        CREATE TABLE IF NOT EXISTS users(
-            first_name TEXT  NOT NULL,
-            last_name TEXT NOT NULL,
-            county TEXT NOT NULL,
-            username TEXT NOT NULL,
-            password TEXT NOT NULL,
-            email TEXT NOT NULL PRIMARY KEY
+        CREATE TABLE IF NOT EXISTS overview(
+            symbol TEXT NOT NULL PRIMARY KEY,
+            assettype TEXT NOT NULL,
+            name TEXT NOT NULL
         );
         '''
     )
-
     is_api_available=HttpSensor(
         task_id="is_api_available",
-        http_conn_id='user_api',
-        endpoint='api/'
+        http_conn_id="finance_api",
+        request_params=defaultParams,
+        endpoint='query?'
     )
 
-    extracting_users=SimpleHttpOperator(
-        task_id="extracting_user",
-        http_conn_id="user_api",
-        endpoint='api/',
+    extracting_task=SimpleHttpOperator(
+        task_id="extracting_company",
+        http_conn_id="finance_api",
+        endpoint="query?",
+        data=defaultParams,
         method="GET",
         response_filter=lambda response:json.loads(response.text),
         log_response=True
     )
 
-    processing_user=PythonOperator(
+    processing_companies=PythonOperator(
         task_id="processing_user",
-        python_callable=_processing_user
+        python_callable=processing_companies
     )
 
     storing_user=BashOperator(
-        task_id='storing_user',
-        bash_command='echo -e ".separator ","\n.import /tmp/processed_user.csv users" | sqlite3 /home/airflow/airflow/airflow.db'
+        task_id="storing_company",
+        bash_command='echo -e ".separator ","\n.import /tmp/companies.csv overview" | sqlite3 /home/airflow/airflow/airflow.db'
         )
 
-    creating_table >> is_api_available >> extracting_users >> processing_user >> storing_user
+
+
+   creating_table >> is_api_available >> extracting_users >> processing_user >> storing_user
 
 
 
